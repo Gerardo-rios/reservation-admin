e68d33f732dde6f6c096ac2cb083e5308f558907
Gerardo-rios <jose.g.rios@unl.edu.ec>
2025-03-11 11:06:33 -0500

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Set both basePath and assetPrefix to your repository name
+  basePath: '/reservation-admin',
+  assetPrefix: '/reservation-admin/',
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
